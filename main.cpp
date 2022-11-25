eaf89e715403e0ed8b8c4654f90e0f8489f1def4
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-11-25 15:59:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int fibonacci (int num)
+{
+    if (num <= 1)
+        return num;
+    return fibonacci(num-1) + fibonacci (num-2);
+}
+
+int main()
+{
+    int num;
+    cin>> num;
+        for (int a =1; a <= num; a++)
+        {
+            cout<<fibonacci(a)<<endl;
+        }}
+    return 0;
 }
